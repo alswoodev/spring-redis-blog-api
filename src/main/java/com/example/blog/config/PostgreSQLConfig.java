ee8ff081e2beb7dedbac6c9631083368d2150d8c
alswoodev <alswoo.dev@gmail.com>
2026-02-13 09:52:16 +0000

    refactor: solve duplication of resolver and add comment

--- src/main/java/com/example/blog/config/PostgreSQLConfig.java
+++ src/main/java/com/example/blog/config/PostgreSQLConfig.java
@@ -5,7 +5,6 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
@@ -20,10 +19,10 @@ public class PostgreSQLConfig {
         sessionFactory.setDataSource(dataSource);
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sessionFactory.setMapperLocations(resolver.getResources("classpath:mappers/*.xml"));
 
-        Resource myBatisConfig = new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml");
-        sessionFactory.setConfigLocation(myBatisConfig);
+        // Set Mappers and Mybatis Config
+        sessionFactory.setMapperLocations(resolver.getResources("classpath:mappers/*.xml"));
+        sessionFactory.setConfigLocation(resolver.getResources("classpath:mybatis-config.xml")[0]);
 
         return sessionFactory.getObject();
     }
